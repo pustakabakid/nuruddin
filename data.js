0f80839b12b0e6af8be04b6f335856fe30ef9e56
pustakabakid <pustakabakid@gmail.com>
2026-09-07 14:59:03 +0700

    feat(audio): set janjisuci.mp3 as wedding background instrument soundtrack

--- data.js
+++ data.js
@@ -49,7 +49,7 @@
     dayName: 'Ahad',
     formattedDate: 'Ahad, 21 September 2026',
     formattedDateShort: '21 • 09 • 2026',
-    audioUrl: '../public/assets/audio/romantic-wedding-soundtrack.mp3',
+    audioUrl: 'janjisuci.mp3',
     quote: {
       arabic: 'وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُم مِّنْ أَنفُسِكُمْ أَزْوَاجًا لِّتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُم مَّوَدَّةً وَرَحْمَةً ۚ إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِّقَوْمٍ يَتَفَكَّرُونَ',
       translation: '"Dan di antara tanda-tanda kebesaran-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir."',
